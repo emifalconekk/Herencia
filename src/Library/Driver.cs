33797c928d78c3590999a8cd5f58771f98374a50
Manuel Risso <Manuel.Risso@correo.ucu.edu.uy>
2025-09-29 12:14:10 -0300

    Created class Passenger and Drivers

---
+++ src/Library/Driver.cs
@@ -0,0 +1,25 @@
+﻿namespace Library;
+
+public class Driver : User
+{
+    private string vehicle;
+    private string bio;
+    private bool type;
+    public string Vehicle
+    {
+        get { return vehicle; }
+        set { Vehicle = value; }
+    }
+
+    public string Bio
+    {
+        get { return bio; }
+        set { Vehicle = value; }
+    }
+
+    public bool Type
+    {
+        get { return type; }
+        set { Type = value; }
+    }
+}
