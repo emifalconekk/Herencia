0d9189b711a02fc1001aaa9d0999666c1da69067
Emilio Falcone <emilio.falcone@correo.ucu.edu.uy>
2025-09-29 11:52:23 -0300

    Created class user

---
+++ src/Library/User.cs
@@ -0,0 +1,34 @@
+﻿namespace Library;
+
+public class User
+{
+    private string name;
+    private int id;
+    private string familyname;
+    private int calification;
+
+    public string Name
+    {
+        get { return name; }
+        set { Name = value; }
+    }
+
+    public int Id
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    public string FamilyName
+    {
+        get { return familyname; }
+        set { familyname = value; }
+    }
+
+    public int Calification
+    {
+        get { return calification; }
+        set { calification = value; }
+    }
+
+}
