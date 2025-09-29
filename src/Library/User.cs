67901a310fec17bc528fa15b1bbb857a56bbfb59
Emilio Falcone <emilio.falcone@correo.ucu.edu.uy>
2025-09-29 13:18:46 -0300

    Halfway through classes full implementation

--- src/Library/User.cs
+++ src/Library/User.cs
@@ -1,16 +1,18 @@
 ﻿namespace Library;
+using System.Collections.Generic;
 
-public class User
+public abstract class User
 {
     private string name;
     private int id;
     private string familyname;
     private int calification;
-
+    public static List<User> users = new List<User>();
+    
     public string Name
     {
         get { return name; }
-        set { Name = value; }
+        set { name = value; }
     }
 
     public int Id
@@ -29,6 +31,15 @@ public class User
     {
         get { return calification; }
         set { calification = value; }
+    }
+
+    protected User(string name, int id, string familyname, int calification)
+    {
+        this.name = name;
+        this.id = id;
+        this.familyname = familyname;
+        this.calification = calification;
+        users.Add(this);
     }
 
 }
